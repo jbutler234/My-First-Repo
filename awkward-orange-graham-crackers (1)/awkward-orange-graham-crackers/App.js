e1c6d5b2f1fcbfa18ddc3bb6cf2599f1683d49bd
jbutler234 <jbutler234@ivytech.edu>
2025-05-07 20:53:42 -0400

    Add files via upload

---
+++ awkward-orange-graham-crackers (1)/awkward-orange-graham-crackers/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hi, my name is Joseph Butler, and I am a mobile developer.</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  text: {
+    fontSize: 18,
+    padding: 20,
+    textAlign: 'center',
+  },
+});
+
